af64ee42c2157c7b88cb2f44f5709d51508c39f2
062173ArcHEr <batsman.sweat-0k@icloud.com>
2025-09-10 19:32:03 +0100

    Implement 数据模型设计

---
+++ data_model_app_0911_0232_cwq.py
@@ -0,0 +1,8 @@
+# 代码生成时间: 2025-09-11 02:32:02
+import tkinter as tk
+from tkinter import messagebox
+
+# 数据模型类
+class DataModel:
+    def __init__(self, data=None):
+        self.data = data if data is not None else {}"
